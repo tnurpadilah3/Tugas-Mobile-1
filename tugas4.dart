0ab64703a65eeb8ed15bad7043803ed144d9c926
tnurpadilah3 <114680197+tnurpadilah3@users.noreply.github.com>
2022-10-14 16:46:29 +0700

    Add files via upload

---
+++ tugas4.dart
@@ -0,0 +1,9 @@
+ void main(List<String> args) {
+  double IPK = 3;
+  String MataKuliah = "Tidak Cumlaude";
+  if(IPK >= 3.5 && MataKuliah =="Tidak Cumlaude"){
+    print("Cumlaude");
+  }else if(IPK < 3.5){
+    print("Tidak Cumlaude");
+  }
+}
