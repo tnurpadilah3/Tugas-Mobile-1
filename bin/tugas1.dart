e1af5f94f935f3f1c83d54ae807e498c26343218
tnurpadilah48@gmail.com <tnurpadilah48@gmail.com>
2022-10-22 10:50:24 +0700

    tugasmobile

---
+++ bin/tugas1.dart
@@ -0,0 +1,10 @@
+ void main(List<String> args) {
+  String Name = "Tasya Nurpadilah";
+  int age = 19;
+  double height = 52;
+  bool isFemale = true;
+
+  print(Name);
+  print(age);
+  print(height);
+}
---
+++ bin/tugas3.dart
@@ -0,0 +1,11 @@
+ void main(List<String> args) {
+  List Hobby = ["Basket", "Renang"];
+  Map<String, dynamic> data = {
+    "Nama" : " Tasya",
+    "Alamat" : "Karawang", 
+  };
+  var dataset = {10,03,04};
+  print(Hobby);
+  print(data);
+  print(dataset);
+}
